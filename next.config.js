e772f522de79de85d34f7846ed817f5ae78b17e8
Mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-10-23 22:32:20 -0300

    feat(config): config svg modules

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextIntl = require('next-intl/plugin')()
+
 const nextConfig = nextIntl({
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: []
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true
+          }
+        }
+      ]
+    })
+    return config
   }
 })
 
