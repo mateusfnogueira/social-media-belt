cf43cbdc2258435cbd88bddb23fe0a63a007ab5c
Mateus.nogueira-extern <mateus.nogueira-extern@renault.com>
2024-10-23 23:43:43 -0300

    feat(pages): create initial pages

---
+++ src/components/seo/head.component.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export function HeadComponent() {
+  return (
+    <Head>
+      <title>Home</title>
+      <meta name="description" content="Home" />
+      {/* <link rel="icon" href="/favicon.ico" /> */}
+    </Head>
+  )
+}
